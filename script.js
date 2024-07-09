f82954f1822d50ad1f33183de8ea0e56efc6e7bb
Hayawi07 <92581951+Hayawi07@users.noreply.github.com>
2024-07-09 14:23:18 +0200

    askldjf
    iocjinoreg

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const totalAmount = 4000;
+    const payments = [200, 200, 200]; // Zaktualizowana lista wpłat
+    const paidAmount = payments.reduce((acc, payment) => acc + payment, 0);
+    const remainingAmount = totalAmount - paidAmount;
+    const progressPercent = ((totalAmount - remainingAmount) / totalAmount) * 100;
+
+    document.getElementById('remaining-amount').textContent = `${remainingAmount} zł`;
+    document.getElementById('paid-amount').textContent = `${paidAmount} zł`;
+
+    const progressBar = document.querySelector('.progress-bar');
+    progressBar.style.width = `${progressPercent}%`;
+    progressBar.setAttribute('data-progress', `${Math.round(progressPercent)}%`);
+});
